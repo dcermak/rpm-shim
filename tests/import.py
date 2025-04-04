cffa4761be4629836ce14b76dc884e6fdcdcebce
Dan Čermák <dcermak@suse.com>
2025-04-04 11:42:27 +0200

    Add a sanity check that the rpm import path is relative to sys.path

--- tests/import.py
+++ tests/import.py
@@ -13,6 +13,19 @@ def test():
     # sanity check
     print("RPM conf dir:", rpm.expandMacro("%getconfdir"))
 
+    from pathlib import Path
+    import sys
+
+    assert len(rpm.__path__) == 1
+    rpm_path = rpm.__path__[0]
+    python_path = sys.path
+
+    for site_dir in python_path:
+        if Path(rpm_path).is_relative_to(Path(site_dir)):
+            break
+    else:
+        raise RuntimeError(f"{rpm_path} is not relative to any python path: {python_path}")
+
 
 if __name__ == "__main__":
     test()
